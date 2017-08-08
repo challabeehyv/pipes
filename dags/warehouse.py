5b4808f5d909a3eb7c7be6ee5f55cc343353530f
Cal Ellowitz <calellowitz@gmail.com>
2017-08-08 13:58:34 -0500

    working app dag

--- dags/warehouse.py
+++ dags/warehouse.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators import BashOperator, PythonOperator
 from datetime import datetime, timedelta
 from uuid import uuid4
 
@@ -26,18 +26,19 @@ default_args = {
 def get_batch_id(*args, **kwargs):
     return uuid4().hex
 
-dag = DAG('update_fact', default_args=default_args, schedule_interval=timedelta(minutes=1))
+dag = DAG('update_fact', default_args=default_args, schedule_interval='@daily')
 
 generate_unique_id = PythonOperator(
     task_id='generate_unique_id',
-    python_callable=get_batch_id
+    python_callable=get_batch_id,
+    dag=dag
 )
 
-create_batch_template = """$CCHQ_HOME/python_env/bin/python manage.py create_batch {{ ti.xcom_pull("generate_unique_id") }} -s {{ ds }} -e {{ tomorrow_ds }}"""
-commit_table_template = """$CCHQ_HOME/python_env/bin/python manage.py commit_table {{ params.table_slug }} {{ ti.xcom_pull("generate_unique_id") }}"""
+create_batch_template = """{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py create_batch {{ ti.xcom_pull("generate_unique_id") }} -s {{ ds }} -e {{ tomorrow_ds }}"""
+commit_table_template = """{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py commit_table {{ params.table_slug }} {{ ti.xcom_pull("generate_unique_id") }}"""
 
 start_batch = BashOperator(
-    task_id='clear_staging_records',
+    task_id='start_batch',
     bash_command=create_batch_template,
     dag=dag
 )
@@ -46,7 +47,7 @@ start_batch = BashOperator(
 update_app_staging = BashOperator(
     task_id='update_dims',
     bash_command=commit_table_template,
-    params={'table_slug': 'application_staging'}
+    params={'table_slug': 'application_staging'},
     dag=dag
 )
 
@@ -58,11 +59,11 @@ update_app_dim = BashOperator(
 )
 
 complete_batch = BashOperator(
-    task_id='complete_batch'
-    bash_command="$CCHQ_HOME/python_env/bin/python manage.py mark_batch complete {{ ti.xcom_pull('generate_unique_id') }}"
+    task_id='complete_batch',
+    bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py mark_batch_complete {{ ti.xcom_pull('generate_unique_id') }}",
     dag=dag
 )
 
 generate_unique_id >> start_batch
-start_batch >> update_location_staging >> update_location_dim
-update_location_dim >> complete_batch
+start_batch >> update_app_staging >> update_app_dim
+update_app_dim >> complete_batch
