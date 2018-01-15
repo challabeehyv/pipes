8d54f8a9b28958c00a95c7c526e6ce12a23856f4
Cal Ellowitz <calellowitz@gmail.com>
2018-01-15 12:07:43 +0530

    fuller poc workflow

--- dags/dim.py
+++ dags/dim.py
@@ -2,9 +2,13 @@ from airflow import DAG
 from airflow.operators import BashOperator
 
 
-def dim_subdag(parent_dag, child_dag, start_date, schedule_interval):
+commit_table_template = """{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py commit_table {{ params.table_slug }} {{ ti.xcom_pull(params.start_id) }}"""
 
-    dim_slug = '{}_dim'.format(child_dag)
+
+def linear_subdag(parent_dag, child_dag, start_date, schedule_interval, final_type):
+
+    final_slug = '{}_{}'.format(child_dag, final_type)
+    staging_slug = '{}_staging'.format(child_dag)
     start_id = 'start_{}_batch'.format(child_dag)
     
     dag = DAG(
@@ -12,8 +16,6 @@ def dim_subdag(parent_dag, child_dag, start_date, schedule_interval):
         start_date=start_date,
         schedule_interval=schedule_interval
     )
-
-    commit_table_template = """{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py commit_table {{ params.table_slug }} {{ ti.xcom_pull("start_batch") }}"""
 
     start_batch = BashOperator(
         task_id=start_id,
@@ -27,21 +29,81 @@ def dim_subdag(parent_dag, child_dag, start_date, schedule_interval):
     update_staging = BashOperator(
         task_id='update_{}_staging'.format(child_dag),
         bash_command=commit_table_template,
-        params={'table_slug': 'application_staging', 'start_id': start_id},
+        params={'table_slug': staging_slug, 'start_id': start_id},
         dag=dag
     )
 
     update_dim = BashOperator(
-        task_id='load_{}_dim'.format(child_dag),
+        task_id='load_{}_{}'.format(child_dag, final_type),
         bash_command=commit_table_template,
-        params={'table_slug': 'application_dim', 'start_id': start_id},
+        params={'table_slug': final_slug, 'start_id': start_id},
         dag=dag
     )
 
     complete_batch = BashOperator(
         task_id='complete_batch',
         bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py mark_batch_complete {{ ti.xcom_pull(params.start_id) }}",
+        params={'start_id': start_id}
         dag=dag
     )
 
     start_batch >> update_staging >> update_dim >> complete_batch
+
+    return dag
+
+
+def dim_subdag(parent_dag, child_dag, start_date, schedule_interval):
+    return linear_subdag(parent_dag, child_dag, start_date, schedule_interval, 'dim')
+
+
+def fact_subdag(parent_dag, child_dag, start_date, schedule_interval):
+    return linear_subdag(parent_dag, child_dag, start_date, schedule_interval, 'fact')
+
+
+def join_subdag(parent_dag, child_dag, start_date, schedule_interval, staging_dependencies):
+
+    start_id = 'start_{}_batch'.format(child_dag)
+    
+    dag = DAG(
+        '{}.{}'.format(parent_dag, child_dag),
+        start_date=start_date,
+        schedule_interval=schedule_interval
+    )
+
+    start_batch = BashOperator(
+        task_id=start_id,
+        bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py create_batch {{ params.dim_slug }}",
+        params={'dim_slug': dim_slug}
+        dag=dag,
+        xcom_push=True
+    )
+
+    tasks = []
+    for table in staging_dependencies:
+        update = BashOperator(
+            task_id='update_{}_staging'.format(table),
+            bash_command=commit_table_template,
+            params={'table_slug': staging_slug, 'start_id': start_id},
+            dag=dag
+        )
+        tasks.append(update)
+        update.set_upstream(start_batch)
+
+    update_dim = BashOperator(
+        task_id='load_{}_dim'.format(child_dag),
+        bash_command=commit_table_template,
+        params={'table_slug': dim_slug, 'start_id': start_id},
+        dag=dag
+    )
+
+    complete_batch = BashOperator(
+        task_id='complete_batch',
+        bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py mark_batch_complete {{ ti.xcom_pull(params.start_id) }}",
+        params={'start_id': start_id}
+        dag=dag
+    )
+    
+    for task in tasks:
+        update_dim.set_upstream(task)
+
+    update_dim >> complete_batch
--- dags/warehouse.py
+++ dags/warehouse.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators import BashOperator
 from airflow.operators.subdag_operator import SubDagOperator
-from dags.dim import dim_subdag
+from dags.dim import dim_subdag, join_subdag, fact_subdag
 
 default_args = {
     'owner': 'cchq',
@@ -25,7 +25,7 @@ DAG_ID = 'update_warehouse'
 dag = DAG(DAG_ID, default_args=default_args, schedule_interval='@daily')
 
 update_app_dim = SubDagOperator(
-    subdag=dim_subdag(DAG_ID, 'app', dag.start_date, dag.schedule_interval),
+    subdag=dim_subdag(DAG_ID, 'application', dag.start_date, dag.schedule_interval),
     task_id='update_app_dim',
     dag=dag
 )
@@ -33,12 +33,6 @@ update_app_dim = SubDagOperator(
 update_user_dim = SubDagOperator(
     subdag=dim_subdag(DAG_ID, 'user', dag.start_date, dag.schedule_interval),
     task_id='update_user_dim',
-    dag=dag
-)
-
-update_location_dim = SubDagOperator(
-    subdag=dim_subdag(DAG_ID, 'location', dag.start_date, dag.schedule_interval),
-    task_id='update_location_dim',
     dag=dag
 )
 
@@ -53,3 +47,32 @@ update_domain_dim = SubDagOperator(
     task_id='update_domain_dim',
     dag=dag
 )
+
+update_form_fact = SubDagOperator(
+    subdag=fact_subdag(DAG_ID, 'form', dag.start_date, dag.schedule_interval),
+    task_id='update_form_fact'
+    dag=dag
+)
+
+update_user_group_dim = SubDagOperator(
+    subdag=join_subdag(DAG_ID, 'user_group', dag.start_date, dag.schedule, ['group']),
+    task_id='update_user_group_dim',
+    dag=dag
+)
+
+clear_staging_records = BashOperator(
+    task_id='clear_staging',
+    bash_command=="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py clear_staging_records",
+    dag=dag
+)
+
+update_user_dim >> update_user_group_dim
+update_group_dim >> update_user_group_dim
+update_user_dim >> update_form_fact
+update_domain_dim >> update_form_fact
+update_app_dim >> clear_staging_records
+update_user_dim >> clear_staging_records
+update_group_dim >> clear_staging_records
+update_domain_dim >> clear_staging_records
+update_form_fact >> clear_staging_records
+udpate_user_group_dim >> clear_staging_records
