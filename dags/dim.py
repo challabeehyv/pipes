88d0866b11659b9e7942281fc8531b33da5fdbd2
Cal Ellowitz <calellowitz@gmail.com>
2018-01-16 18:08:39 +0530

    creating a fuller warehouse dag and fixing errors

--- dags/dim.py
+++ dags/dim.py
@@ -5,7 +5,7 @@ from airflow.operators import BashOperator
 commit_table_template = """{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py commit_table {{ params.table_slug }} {{ ti.xcom_pull(params.start_id) }}"""
 
 
-def linear_subdag(parent_dag, child_dag, start_date, schedule_interval, final_type):
+def linear_subdag(parent_dag, child_dag, default_args, schedule_interval, final_type):
 
     final_slug = '{}_{}'.format(child_dag, final_type)
     staging_slug = '{}_staging'.format(child_dag)
@@ -13,14 +13,14 @@ def linear_subdag(parent_dag, child_dag, start_date, schedule_interval, final_ty
     
     dag = DAG(
         '{}.{}'.format(parent_dag, child_dag),
-        start_date=start_date,
+        default_args=default_args,
         schedule_interval=schedule_interval
     )
 
     start_batch = BashOperator(
         task_id=start_id,
-        bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py create_batch {{ params.dim_slug }}",
-        params={'dim_slug': dim_slug}
+        bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py create_batch {{ params.table_slug }}",
+        params={'table_slug': final_slug},
         dag=dag,
         xcom_push=True
     )
@@ -43,7 +43,7 @@ def linear_subdag(parent_dag, child_dag, start_date, schedule_interval, final_ty
     complete_batch = BashOperator(
         task_id='complete_batch',
         bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py mark_batch_complete {{ ti.xcom_pull(params.start_id) }}",
-        params={'start_id': start_id}
+        params={'start_id': start_id},
         dag=dag
     )
 
@@ -52,28 +52,30 @@ def linear_subdag(parent_dag, child_dag, start_date, schedule_interval, final_ty
     return dag
 
 
-def dim_subdag(parent_dag, child_dag, start_date, schedule_interval):
-    return linear_subdag(parent_dag, child_dag, start_date, schedule_interval, 'dim')
+def dim_subdag(parent_dag, child_dag, default_args, schedule_interval):
+    return linear_subdag(parent_dag, child_dag, default_args, schedule_interval, 'dim')
 
 
-def fact_subdag(parent_dag, child_dag, start_date, schedule_interval):
-    return linear_subdag(parent_dag, child_dag, start_date, schedule_interval, 'fact')
+def fact_subdag(parent_dag, child_dag, default_args, schedule_interval):
+    return linear_subdag(parent_dag, child_dag, default_args, schedule_interval, 'fact')
 
 
-def join_subdag(parent_dag, child_dag, start_date, schedule_interval, staging_dependencies):
+def join_subdag(parent_dag, child_dag, default_args, schedule_interval, staging_dependencies):
 
     start_id = 'start_{}_batch'.format(child_dag)
+    dim_slug = '{}_dim'.format(child_dag)
+    staging_slug = '{}_staging'.format(child_dag)
     
     dag = DAG(
         '{}.{}'.format(parent_dag, child_dag),
-        start_date=start_date,
+        default_args=default_args,
         schedule_interval=schedule_interval
     )
 
     start_batch = BashOperator(
         task_id=start_id,
         bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py create_batch {{ params.dim_slug }}",
-        params={'dim_slug': dim_slug}
+        params={'dim_slug': dim_slug},
         dag=dag,
         xcom_push=True
     )
@@ -99,7 +101,7 @@ def join_subdag(parent_dag, child_dag, start_date, schedule_interval, staging_de
     complete_batch = BashOperator(
         task_id='complete_batch',
         bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py mark_batch_complete {{ ti.xcom_pull(params.start_id) }}",
-        params={'start_id': start_id}
+        params={'start_id': start_id},
         dag=dag
     )
     
@@ -107,3 +109,5 @@ def join_subdag(parent_dag, child_dag, start_date, schedule_interval, staging_de
         update_dim.set_upstream(task)
 
     update_dim >> complete_batch
+
+    return dag
--- dags/warehouse.py
+++ dags/warehouse.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.operators import BashOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.subdag_operator import SubDagOperator
-from dags.dim import dim_subdag, join_subdag, fact_subdag
+from dim import dim_subdag, join_subdag, fact_subdag
 
 default_args = {
     'owner': 'cchq',
@@ -25,44 +25,44 @@ DAG_ID = 'update_warehouse'
 dag = DAG(DAG_ID, default_args=default_args, schedule_interval='@daily')
 
 update_app_dim = SubDagOperator(
-    subdag=dim_subdag(DAG_ID, 'application', dag.start_date, dag.schedule_interval),
-    task_id='update_app_dim',
+    subdag=dim_subdag(DAG_ID, 'application', dag.default_args, dag.schedule_interval),
+    task_id='application',
     dag=dag
 )
 
 update_user_dim = SubDagOperator(
-    subdag=dim_subdag(DAG_ID, 'user', dag.start_date, dag.schedule_interval),
-    task_id='update_user_dim',
+    subdag=dim_subdag(DAG_ID, 'user', dag.default_args, dag.schedule_interval),
+    task_id='user',
     dag=dag
 )
 
 update_group_dim = SubDagOperator(
-    subdag=dim_subdag(DAG_ID, 'group', dag.start_date, dag.schedule_interval),
-    task_id='update_group_dim',
+    subdag=dim_subdag(DAG_ID, 'group', dag.default_args, dag.schedule_interval),
+    task_id='group',
     dag=dag
 )
 
 update_domain_dim = SubDagOperator(
-    subdag=dim_subdag(DAG_ID, 'domain', dag.start_date, dag.schedule_interval),
-    task_id='update_domain_dim',
+    subdag=dim_subdag(DAG_ID, 'domain', dag.default_args, dag.schedule_interval),
+    task_id='domain',
     dag=dag
 )
 
 update_form_fact = SubDagOperator(
-    subdag=fact_subdag(DAG_ID, 'form', dag.start_date, dag.schedule_interval),
-    task_id='update_form_fact'
+    subdag=fact_subdag(DAG_ID, 'form', dag.default_args, dag.schedule_interval),
+    task_id='form',
     dag=dag
 )
 
 update_user_group_dim = SubDagOperator(
-    subdag=join_subdag(DAG_ID, 'user_group', dag.start_date, dag.schedule, ['group']),
-    task_id='update_user_group_dim',
+    subdag=join_subdag(DAG_ID, 'user_group', dag.default_args, dag.schedule_interval, ['group']),
+    task_id='user_group',
     dag=dag
 )
 
 clear_staging_records = BashOperator(
     task_id='clear_staging',
-    bash_command=="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py clear_staging_records",
+    bash_command="{{ var.value.CCHQ_HOME }}/python_env/bin/python {{ var.value.CCHQ_HOME }}/manage.py clear_staging_records",
     dag=dag
 )
 
@@ -75,4 +75,4 @@ update_user_dim >> clear_staging_records
 update_group_dim >> clear_staging_records
 update_domain_dim >> clear_staging_records
 update_form_fact >> clear_staging_records
-udpate_user_group_dim >> clear_staging_records
+update_user_group_dim >> clear_staging_records
